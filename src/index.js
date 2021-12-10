84e906728c5ea3e8036fa212d1c89f8d7ac8ee0e
Sulaiman <suleiman_hayek@yahoo.com>
2021-12-10 20:28:27 +0300

    add states

--- src/index.js
+++ src/index.js
@@ -4,23 +4,38 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {value: null,
-        };
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {value: null,
+    //     };
+    // }
     render() {
         return (
-            <button className="square" onClick={()=> this.setState({value: 'X'})}>
-                {this.state.value}
+            <button className="square" onClick={()=> this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            Squares: Array(9).fill(null),
+        };
+    }
+    handleClick(i){
+        const squares = this.state.Squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: });
+
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+        <Square value={this.state.Squares[i]} 
+        onClick={() => this.handleClick(i)}/>
+        );
     }   
 
     render() {
