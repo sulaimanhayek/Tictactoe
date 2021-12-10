1009e861d8aa64d4fc71d87843660adb6ef6da11
Sulaiman <suleiman_hayek@yahoo.com>
2021-12-10 17:01:40 +0300

    rendre square

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
@@ -15,8 +15,8 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
-    }
+        return <Square value={i} />;
+    }   
 
     render() {
         const status = 'Next player: X';
